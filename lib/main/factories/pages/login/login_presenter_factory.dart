ef98cc88ca803c9c19605d577593b25661f76e2b
Rodney Neville <rodneyneville@gmail.com>
2021-12-27 16:30:39 -0300

    Vai

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -1,9 +1,15 @@
 import '../../../../presentation/presenters/presenters.dart';
-import '../../../../ui/pages/login/login_presenter.dart';
+import '../../../../ui/pages/pages.dart';
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+      authentication: makeRemoteAuthentication(),
+      validation: makeLoginValidation());
+}
+
+LoginPresenter makeGextLoginPresenter() {
+  return GetxLoginPresenter(
       authentication: makeRemoteAuthentication(),
       validation: makeLoginValidation());
 }
